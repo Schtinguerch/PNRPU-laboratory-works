a971acce035f5e8b1877744b3bda600185493e00
Schtinguerch <schtinguerch@gmail.com>
2021-02-03 21:54:56 +0500

    Implementation of main function is done

--- Lab.work 9 - String input-output/main.cpp
+++ Lab.work 9 - String input-output/main.cpp
@@ -4,23 +4,72 @@
 
 using namespace std;
 
-bool is_word_letter(char x)
+bool IsWordLetter(char x)
+{
+    x = tolower(x);
+    bool is_letter = ((x >= 'a') && (x <= 'z')) || ((x >= 'а') && (x <= 'я'));
+
+    return is_letter;
+}
+
+int WordLength(string word)
 {
 
 }
 
-int word_length(string word)
-{
-
-}
-
-int shortest_word_length(string expression) 
+int ShortWordLength(string expression) 
 {
 
 }
 
 int main()
 {
-    //TODO: write code here
+    int shortestWordIndex, 
+        shortestWordLength = INT_MAX,
+        workIndex = 0;
+
+    ifstream inputFile("Work files\\F1.txt");
+    ofstream outputFile("Work files\\F2.txt");
+
+    if (inputFile.is_open()) 
+    {
+        while (!inputFile.eof())
+        {
+            workIndex++;
+
+            string row;
+            getline(inputFile, row);
+
+            int minLength = ShortWordLength(row);
+            if (minLength < shortestWordLength)
+            {
+                shortestWordIndex = workIndex;
+                shortestWordLength = minLength;
+            } 
+        }
+
+        workIndex = 0;
+        inputFile.seekg(ios_base::beg);
+
+        while (!inputFile.eof())
+        {
+            workIndex++;
+
+            string row;
+            getline(inputFile, row);
+
+            if (workIndex != shortestWordIndex)
+                outputFile << row << endl;
+        }
+
+        cout << "String row №" << shortestWordIndex << "has the shortest word!!!";
+    }
+    else 
+    {
+        cout << "Error: the file is not open!!!" << endl << endl
+            << "Please, check does Work files\\F1.txt exist"
+            << "and check the file status (is the file read only or protected)";
+    }
+
     return 0;
 }
