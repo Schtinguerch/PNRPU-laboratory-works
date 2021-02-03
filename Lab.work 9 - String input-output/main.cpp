a11d2af1cc463a90b527ae1e5c2a54b74a39e441
Schtinguerch <schtinguerch@gmail.com>
2021-02-03 19:32:28 +0500

    Added declarations of functions, which will help to solve the task

--- Lab.work 9 - String input-output/main.cpp
+++ Lab.work 9 - String input-output/main.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+bool is_word_letter(char x)
+{
+
+}
+
+int word_length(string word)
+{
+
+}
+
+int shortest_word_length(string expression) 
+{
+
+}
+
 int main()
 {
     //TODO: write code here
